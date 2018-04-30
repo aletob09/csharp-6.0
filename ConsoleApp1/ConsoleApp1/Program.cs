b67bc82c11e681afd1af719a4decfcefd59c12e8
Alexander Strauss <alexander.strauss@ecx.io>
2018-04-30 09:41:15 +0200

    new folder

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    struct Time
+    {
+        public int s;
+
+        public Time(int sec, int min, int hour)
+        {
+            s = (3600 * hour + 60 * min + sec) % 86400;
+        }
+
+        public Time(int sec)
+        {
+            s = sec;
+        }
+
+        int ConvertTime(int)
+        {
+            if (s > 59)
+            {
+               
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-           
+
         }
     }
 }
