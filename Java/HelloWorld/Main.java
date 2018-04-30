7d02e3244e42b3b0547688bdc663b716ff82d569
Alexander Strauss <alexander.strauss@ecx.io>
2018-04-30 16:34:47 +0200

    java files

---
+++ Java/HelloWorld/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        //int[] arr = new ArrayBlockingQueue<>(5);
+        int[] arr = new int[5];
+
+        for(int x = 0; x <= arr.length; x++)
+        {
+            System.out.println("test" + x);
+        }
+
+        System.out.println(arr.length);
+    }
+    
+}   
