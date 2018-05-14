12cb00aaf9fb944056908d7529a1665a47cb42fc
Alexander Strauss <alexander.strauss@ecx.io>
2018-05-14 10:24:28 +0200

    added battle method

--- Java/Champs/Main.java
+++ Java/Champs/Main.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+import java.math.*;
 
 /**
  * Main
@@ -8,23 +9,31 @@ public class Main {
     public static void main(String[] args) {
         
         List<Champion> champs = new ArrayList<>();
-        champs.add(new Tank("Galio", 50, 24, 32));
-        champs.add(new ADC("Jinx", 50, 20, 40));
+        champs.add(new Tank("Galio", 500, 50, 24, 32));
+        champs.add(new ADC("Jinx", 100, 50, 20, 40));
         //System.out.print((champs.get(0)).name);
         //System.out.print((champs.get(1)).name);
         champs.get(0).levelUp(2);
         
 
         
-        /*
-        ADC Jhin = new ADC("Jhin", 80, 40, 20);
-        Jhin.levelUp(3);
-        System.out.println(Jhin.speed);
+        
+        ADC Jhin = new ADC("Jhin", 150, 80, 80, 1);
+        //Jhin.levelUp(3);
         
 
-        Tank Nasus = new Tank("Nasus", 50, 50, 20);
-        Nasus.levelUp(2);
-        System.out.println(Nasus.armor);
-        */
+        Tank Nasus = new Tank("Nasus", 800, 50, 50, 20);
+        //Nasus.levelUp(2);
+
+        System.out.print((battle(Jhin, Nasus)));
+        
+        
+    }
+
+
+    public static String battle(ADC adc, Tank tank){
+        double tmp = tank.health / (adc.calcDPS() - tank.armor);
+        String foo = tank.name + " will be dead after " + tmp + " seconds if fighting against " + adc.name;
+        return foo;
     }
 }
