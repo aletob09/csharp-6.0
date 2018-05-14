b374f2eb72fd9754e604b44c60cec76368f1db89
Alexander Strauss <alexander.strauss@ecx.io>
2018-05-14 10:29:15 +0200

    added health calculation

--- Java/Champs/Champion.java
+++ Java/Champs/Champion.java
@@ -1,9 +1,16 @@
 public class Champion {
     String name;
+    int health;
     int growthRate;
     int Level = 1;
 
     protected void levelUp(int addLVL){
         return; 
+    }
+
+    protected int calcHPGrowth(){
+        int tmp = health + (health / (100 / growthRate));
+
+        return tmp;
     }
 }
